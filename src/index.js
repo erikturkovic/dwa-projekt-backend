5c0c0c630c5816c9514e0f10e89ce9ff287022dc
erikturkovic <eturkovic@student.unipu.hr>
2022-06-09 20:23:56 +0200

    Hello world test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("woah")
+import express from "express";
+console.log("works")
+
+const app = express();
+const port = 3000;
+console.log("expres + ")
+console.log("3000 + ")
+
+app.get("/",(req,res)=>{
+    res.send("hello za warudo!");
+});
+
+app.listen(port,()=>{
+    console.log("listening on port ${port}")
+});
+
