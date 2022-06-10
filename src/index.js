4fcd6cb5e51844bcc32502c62a6342787033e1a2
erikturkovic <eturkovic@student.unipu.hr>
2022-06-10 11:44:42 +0200

    Get/Post/Patch/Delete za detalje studenta i biznisa

--- src/index.js
+++ src/index.js
@@ -79,9 +79,9 @@ app.delete("/poslodavci/:id_poslodavca",(req, res)=>{
 //get test za ponude
 app.get("/ponude",(req,res)=>{
     let ponude = [
-        {id_ponude: 1, id_poslodavca:1, posao:"intern" , jmbag:[1,2]},
-        {id_ponude: 2, id_poslodavca:2, posao:"konobar", jmbag:[2,3]},
-        {id_ponude: 3, id_poslodavca:3, posao:"dostavljač" , jmbag:[3,1]}
+        {id_ponude: 1, id_poslodavca:1, posao:"intern" , prijavljeni:[{jmbag:1},{jmbag:2}]},
+        {id_ponude: 2, id_poslodavca:2, posao:"konobar", prijavljeni:[{jmbag:2},{jmbag:3}]},
+        {id_ponude: 3, id_poslodavca:3, posao:"dostavljač" , prijavljeni:[{jmbag:3},{jmbag:1}]}
     ];
     res.status(200);
     res.send(ponude);
@@ -108,8 +108,70 @@ app.delete("/ponude/:id_ponude",(req, res)=>{
     res.send();
 })
 
-
-app.listen(port,()=>{
-    console.log("listening on port")
+//get test za detalje studenta
+app.get("/studenti/detalji",(req,res)=>{
+    let detalji_studenta = [
+    {jmbag:1, fakultet: "a bo", godina:"3",studij:"rač"},
+    {jmbag:2, fakultet: "a bo", godina:"1",studij:"stro"},
+    {jmbag:3, fakultet: "a bo", godina:"2",studij:"inf"}
+    ];
+    res.status(200);
+    res.send(detalji_studenta);
 });
+
+//post test za detalje studenta
+app.post("/studenti/detalji",(req,res)=>{
+    console.log("Podaci",req.body);
+    res.status(201);
+    res.send();
+});
+
+//patch test za detalje studenta
+app.patch("/studenti/detalji/:jmbag",(req, res)=>{
+    console.log("Podaci", req.params.jmbag, req.body);
+    res.status(200);
+    res.send();
+});
+
+//delete test za ponude
+app.delete("/studenti/detalji/:jmbag",(req, res)=>{
+    console.log("Podaci", req.params.jmbag);
+    res.status(200);
+    res.send();
+})
+
+//get test za detalje biznisa
+app.get("/biznis",(req,res)=>{
+    let biznis = [
+    {id_biznisa:1, id_poslodavca: 1 , ime_biznisa:"wow.d.o.o.", mjesto_hq:"Pula"},
+    {id_biznisa:2, id_poslodavca: 2 , ime_biznisa:"hmm.d.o.o.", mjesto_hq:"Pula"},
+    {id_biznisa:3, id_poslodavca: 3, ime_biznisa:"hah.d.o.o.", mjesto_hq:"Pula"}
+    ];
+    res.status(200);
+    res.send(biznis);
+});
+
+//post test za detalje biznisa
+app.post("/biznis",(req,res)=>{
+    console.log("Podaci",req.body);
+    res.status(201);
+    res.send();
+});
+
+//patch test za detalje biznisa
+app.patch("/biznis/:id_biznisa",(req, res)=>{
+    console.log("Podaci", req.params.id_biznisa, req.body);
+    res.status(200);
+    res.send();
+});
+
+//delete test za ponude
+app.delete("/biznis/:id_biznisa",(req, res)=>{
+    console.log("Podaci", req.params.id_biznisa);
+    res.status(200);
+    res.send();
+})
+
+app.listen(port, () => console.log(`Slušam na portu ${port}`));
+
 
