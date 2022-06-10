5bef3719b79be2308e36550c54c5afc7fd641887
erikturkovic <eturkovic@student.unipu.hr>
2022-06-10 07:25:29 +0200

    Insomnia test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import express from "express";
+import cors from "cors";
 console.log("works")
 
 const app = express();
 const port = 3000;
-console.log("expres + ")
-console.log("3000 + ")
+
+app.use(express.json());
+app.use(cors());
+
 
 app.get("/",(req,res)=>{
     res.send("hello za warudo!");
+});
+
+app.get("/studenti",(req,res)=>{
+    let studenti = [
+        {jmbag: 1, mail: "mail@mail.com", ime:"Ivan", prezime:"Ivica"},
+        {jmbag: 2, mail: "snail@snail.com", ime:"Pero", prezime:"Perica"}
+    ];
+    res.status(200);
+    res.send(studenti);
 });
 
 app.listen(port,()=>{
