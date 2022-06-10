4f93bdd48f6d5034c9bfc79a0aa2e257f0e1cab6
erikturkovic <eturkovic@student.unipu.hr>
2022-06-10 11:07:47 +0200

    Get/Post/Patch/Delete ponude

--- src/index.js
+++ src/index.js
@@ -47,9 +47,9 @@ app.delete("/studenti/:jmbag",(req, res)=>{
 //get test za poslodavce
 app.get("/poslodavci",(req,res)=>{
     let poslodavci = [
-        {id: 1, mail: "lol@lol.com", ime:"Roko", prezime:"Rokic"},
-        {id: 2, mail: "lmao@lmao.com", ime:"Marko", prezime:"Markic"},
-        {id: 3, mail: "rofl@rofl.com", ime:"Filip", prezime:"Fico"}
+        {id_poslodavca: 1, mail: "lol@lol.com", ime:"Roko", prezime:"Rokic"},
+        {id_poslodavca: 2, mail: "lmao@lmao.com", ime:"Marko", prezime:"Markic"},
+        {id_poslodavca: 3, mail: "rofl@rofl.com", ime:"Filip", prezime:"Fico"}
     ];
     res.status(200);
     res.send(poslodavci);
@@ -63,18 +63,51 @@ app.post("/poslodavci",(req,res)=>{
 });
 
 //patch test za poslodavce
-app.patch("/poslodavci/:id",(req, res)=>{
-    console.log("Podaci", req.params.id, req.body);
+app.patch("/poslodavci/:id_poslodavca",(req, res)=>{
+    console.log("Podaci", req.params.id_poslodavca, req.body);
     res.status(200);
     res.send();
 });
 
-//delete test za poslodave
-app.delete("/poslodavci/:id",(req, res)=>{
-    console.log("Podaci", req.params.id);
+//delete test za poslodavce
+app.delete("/poslodavci/:id_poslodavca",(req, res)=>{
+    console.log("Podaci", req.params.id_poslodavca);
     res.status(200);
     res.send();
 })
+
+//get test za ponude
+app.get("/ponude",(req,res)=>{
+    let ponude = [
+        {id_ponude: 1, id_poslodavca:1, posao:"intern" , jmbag:[1,2]},
+        {id_ponude: 2, id_poslodavca:2, posao:"konobar", jmbag:[2,3]},
+        {id_ponude: 3, id_poslodavca:3, posao:"dostavljač" , jmbag:[3,1]}
+    ];
+    res.status(200);
+    res.send(ponude);
+});
+
+//post test za ponude
+app.post("/ponude",(req,res)=>{
+    console.log("Podaci",req.body);
+    res.status(201);
+    res.send();
+});
+
+//patch test za ponude
+app.patch("/ponude/:id_ponude",(req, res)=>{
+    console.log("Podaci", req.params.id_ponude, req.body);
+    res.status(200);
+    res.send();
+});
+
+//delete test za ponude
+app.delete("/ponude/:id_ponude",(req, res)=>{
+    console.log("Podaci", req.params.id_ponude);
+    res.status(200);
+    res.send();
+})
+
 
 app.listen(port,()=>{
     console.log("listening on port")
