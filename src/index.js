2945c6ba3786200a4253ee0cf4a72a400f5f6dbc
erikturkovic <eturkovic@student.unipu.hr>
2022-09-26 22:26:08 +0200

    'heroku'

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import auth from "./auth.js";
 import connect from "./db.js";
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
